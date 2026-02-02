5c3d58ccac660da5cbd35c3d996bc1b62f828446
russeldongmo <russeldongmo96@gmail.com>
2026-02-02 07:24:01 +0100

    Add Railway configuration

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,11 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  
+  // log pour debug
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
