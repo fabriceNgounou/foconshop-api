061b00a1fb1cc33b2ec9886e19f969d5a887065c
Fredie T <103999729+FredieBrunn@users.noreply.github.com>
2026-02-02 15:50:45 +0100

    Enable CORS for the application

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,13 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
