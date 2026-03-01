8e2a992002307ff6d16b187df17a31ab5e387915
russeldongmo <russeldongmo96@gmail.com>
2026-03-01 15:47:43 +0100

    gestion promotions

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { PromotionModule } from 'src/promotion/promotion.module';
 
 @Module({
+  imports: [PromotionModule],
   controllers: [CartController],
   providers: [CartService,],
 })
